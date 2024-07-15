91e27f9c3379b044f86cbe5edc45986577a3a811
Dipanwita Biswas <biswasdipanwita.2001@gmail.com>
2024-07-15 17:58:39 +0530

    first commit

---
+++ medico/src/component/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <>
+        <div className='p-1 bg-cyan-700 w-full h-16 flex justify-center items-center'>
+            <h1 className='text-white'>this is navbar</h1>
+        </div>
+      
+    </>
+  )
+}
+
+export default Navbar
